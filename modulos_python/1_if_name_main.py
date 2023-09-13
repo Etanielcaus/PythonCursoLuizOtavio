272506b41ce593ed4684cb89666253d60c0f1b86
Etanielcaus <nielfilosofia@gmail.com>
2023-09-13 06:08:09 -0300

    att

---
+++ modulos_python/1_if_name_main.py
@@ -0,0 +1,4 @@
+from modulo import soma
+
+print(soma(10, 5))
+print(__name__)
---
+++ modulos_python/modulo.py
@@ -0,0 +1,8 @@
+# este módulo é para a aula 1
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+if __name__ == '__main__':
+    print(soma(10, 30))
+print(__name__)
