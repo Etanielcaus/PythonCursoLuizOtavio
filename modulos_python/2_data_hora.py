9fb1759db10bed4e25191a6d5cc98934777dc42a
Etanielcaus <nielfilosofia@gmail.com>
2023-09-13 07:07:44 -0300

    att

--- modulos_python/2_data_hora.py
+++ modulos_python/2_data_hora.py
@@ -11,6 +11,9 @@
 # Instalando o pytz
 # pip install pytz types-pytz
 from datetime import datetime
+from pytz import timezone
+from dateutil.relativedelta import relativedelta
+
 
 data_str_data = '2022/04/20 07:49:23'
 data_str_data = '20/04/2022'
@@ -19,3 +22,62 @@ data_str_fmt = '%d/%m/%Y'
 # data = datetime(2022, 4, 20, 7, 49, 23)
 data = datetime.strptime(data_str_data, data_str_fmt)
 print(data)
+print('-' * 80)
+
+# ----------------------------------------------------------------
+# Timezone
+
+data = datetime.now(timezone('America/Sao_Paulo'))  # Horário atual na timezone
+print(data)
+print('-' * 80)
+
+# ----------------------------------------------------------------
+# Timestamp
+
+data = datetime.now()
+print(data.timestamp())  # Segundos até hoje
+print(datetime.fromtimestamp(data.timestamp()))
+print('-' * 80)
+
+# ----------------------------------------------------------------
+# Calculando datas
+
+# datetime.timedelta e dateutil.relativetimedelta (calculando datas)
+# Docs:
+# https://dateutil.readthedocs.io/en/stable/relativedelta.html
+# https://docs.python.org/3/library/datetime.html#timedelta-objects
+
+fmt = '%d/%m/%Y %H:%M:%S'
+data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
+data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+# delta = timedelta(days=10, hours=2)
+delta = relativedelta(data_fim, data_inicio)
+print(delta.days, delta.years)
+print(data_fim - delta)
+# print(data_fim)
+print(data_fim + relativedelta(seconds=60, minutes=10))
+
+# delta = data_fim - data_inicio
+# print(delta.days, delta.seconds, delta.microseconds)
+# print(delta.total_seconds())
+# print(data_fim > data_inicio)
+# print(data_fim < data_inicio)
+# print(data_fim == data_inicio)
+print('-' * 80)
+
+# ----------------------------------------------------------------
+# Formatando datas
+# datetime.strftime('DATA', 'FORMATO')
+# https://docs.python.org/3/library/datetime.html
+
+# data = datetime(2022, 12, 13, 7, 59, 23)
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+print(data.strftime('%d/%m/%Y'))
+print(data.strftime('%d/%m/%Y %H:%M'))
+print(data.strftime('%d/%m/%Y %H:%M:%S'))
+print(data.strftime('%Y'), data.year)
+print(data.strftime('%d'), data.day)
+print(data.strftime('%m'), data.month)
+print(data.strftime('%H'), data.hour)
+print(data.strftime('%M'), data.minute)
+print(data.strftime('%S'), data.second)
--- modulos_python/README.md
+++ modulos_python/README.md
@@ -3,23 +3,7 @@ Aqui vai se estudar os seguintes módulos:
 
 1-if__name__=="__main__"
 
-2-data e hora com python(datetime)
-Criando datas com módulo datetime
-
-datetime(ano, mês, dia)
-
-datetime(ano, mês, dia, horas, minutos, segundos)
-
-datetime.strptime('DATA', 'FORMATO')
-
-datetime.now()
-
-https://pt.wikipedia.org/wiki/Era_Unix
-
-datetime.fromtimestamp(Unix Timestamp)
-
-https://docs.python.org/3/library/datetime.html
-
-Para timezones
-
-https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+2-data e hora com python
+Utilizando datetime, Timestamp, pytz e timezones
+Calculando datas com datetime.timedelta e dateutil.relativetimedelta 
+Formatação de datas com datetime
